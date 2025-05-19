fb3acb858139b0c358ff9a78be75970fbdd40f90
Nguyễn Thái Dương <loswkpq17@gmail.com>
2025-05-20 02:41:27 +0700

    server: update schema

---
+++ guru_server/src/api/varriant/controllers/varriant.ts
@@ -0,0 +1,7 @@
+/**
+ * varriant controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::varriant.varriant');
---
+++ guru_server/src/api/varriant/routes/varriant.ts
@@ -0,0 +1,7 @@
+/**
+ * varriant router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::varriant.varriant');
