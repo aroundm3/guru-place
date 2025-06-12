6f8f97e93abedb3eb40e652faabe7eba9906c585
Nguyễn Thái Dương <loswkpq17@gmail.com>
2025-06-12 20:59:29 +0700

    update

--- guru_server/config/server.ts
+++ guru_server/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("APP_URL"),
+  url: `http://${env("APP_URL")}`, // 👈 Thêm http:// ở đây
   app: {
     keys: env.array("APP_KEYS"),
   },
