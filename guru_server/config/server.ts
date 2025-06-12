6d4e956fee660e779e7cd4ed7926446f61b6c83f
Nguyễn Thái Dương <loswkpq17@gmail.com>
2025-06-12 22:34:51 +0700

    update

--- guru_server/config/server.ts
+++ guru_server/config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  host: env("APP_URL", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: `http://${env("APP_HOST")}:${env("APP_PORT")}`, // 👈 Ghép đúng IP + Port
   app: {
     keys: env.array("APP_KEYS"),
   },
